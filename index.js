4f00dd5184bed9ec09911631de1154748ef989f3
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-08-03 11:17:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Follow along with the examples here
+
+
+
+function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
+
+  console.log(say("Howdy", "partner"));
+
+
+function add(num1, num2) {
+    return num1 +num2
+}
+  const num1 = 4;
+  const num2 = 6;
+  const sum = add(num1, num2);
+const message = `The sum of your numbers is: ${sum}.`;
+console.log(message);
